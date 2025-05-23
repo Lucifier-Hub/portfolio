7aa42eecc9501faf606a400243b22e6ea5c16218
Abhijith <abhijith.venu199@gmail.com>
2025-05-23 13:42:12 +0530

    content changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     CommonModule,
     NavbarComponent,
@@ -19,6 +21,7 @@ import { FooterComponent } from './components/footer/footer.component';
     AboutComponent,
     ProjectsComponent,
     SkillsComponent,
+    ExperienceComponent,
     ContactComponent,
     FooterComponent
   ],
@@ -27,13 +30,14 @@ import { FooterComponent } from './components/footer/footer.component';
     <main>
       <app-hero></app-hero>
       <app-about></app-about>
+      <app-experience></app-experience>
       <app-projects></app-projects>
       <app-skills></app-skills>
       <app-contact></app-contact>
     </main>
     <app-footer></app-footer>
   `,
-  styleUrls: ['./app.component.scss']
+  styles: []
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
